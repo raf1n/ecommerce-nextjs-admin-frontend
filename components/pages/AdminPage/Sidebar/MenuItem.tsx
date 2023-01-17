ce61943d283e7fffe973dfb43de14cb2595e28ba
rahatkabir007 <rahatkabir04@gmail.com>
2023-01-17 12:24:18 +0600

    d

--- components/pages/AdminPage/Sidebar/MenuItem.tsx
+++ components/pages/AdminPage/Sidebar/MenuItem.tsx
@@ -18,43 +18,10 @@ interface Props {
 const MenuItem: React.FC<Props> = (props) => {
   const states = useSelector(() => controller.states);
   const router = useRouter();
-<<<<<<< HEAD
   const [nestedMenuActive, setNestedMenuActive] = useState<null | number>(null)
-=======
->>>>>>> 32c751229c362428aa98efcfddcb6bcfb01a3968
 
   const { asPath } = router;
 
-<<<<<<< HEAD
-  const handleClick = () => {
-    if (menuOpen === idx) {
-      setMenuOpen(null);
-      setNestedMenuActive(null)
-    } else {
-      setMenuOpen(idx);
-      setNestedMenuActive(null)
-    }
-  };
-  const handleNestedMenuClick = (index: number) => {
-    if (nestedMenuActive === index) {
-      setNestedMenuActive(null)
-    }
-    else {
-      setNestedMenuActive(index)
-    }
-  }
-
-  return (
-    <li
-      className={`grid cursor-pointer text-sm ${menuOpen === idx ? "bg-[#f8fafb]" : ""
-        }`}
-    >
-      <div
-        onClick={handleClick}
-        className={`${!open ? "min-h-[65px]" : "min-h-[50px]"
-          } duration-300 relative px-5 hover:bg-[#f8fafb] ${menuOpen === idx ? "text-[#6777ef]" : ""
-          }`}
-=======
   const { menu, open, idx, menuOpen, handleMenuClick } = props;
 
   return (
@@ -64,18 +31,14 @@ const MenuItem: React.FC<Props> = (props) => {
           router.push(menu.link)
         }
       }}
-      className={`grid cursor-pointer text-sm ${
-        menuOpen === idx ? "bg-[#f8fafb]" : ""
-      }`}
+      className={`grid cursor-pointer text-sm ${menuOpen === idx ? "bg-[#f8fafb]" : ""
+        }`}
     >
       <div
         onClick={() => handleMenuClick(menuOpen, idx)}
-        className={`${
-          !open ? "min-h-[65px]" : "min-h-[50px]"
-        } duration-300 relative px-5 hover:bg-[#f8fafb] ${
-          menuOpen === idx ? "text-[#6777ef]" : ""
-        }`}
->>>>>>> 32c751229c362428aa98efcfddcb6bcfb01a3968
+        className={`${!open ? "min-h-[65px]" : "min-h-[50px]"
+          } duration-300 relative px-5 hover:bg-[#f8fafb] ${menuOpen === idx ? "text-[#6777ef]" : ""
+          }`}
       >
         <div
           className={`w-full group relative h-full flex items-center ${open && "gap-x-6"
@@ -102,15 +65,8 @@ const MenuItem: React.FC<Props> = (props) => {
       </div>
       {menu.nestedRoutes && (
         <div
-<<<<<<< HEAD
-          className={`${menuOpen === idx ? `h-[${menu.height}]` : "h-0 invisible opacity-0"
+          className={`${menuOpen === idx ? `h-[${menu.height}] visible opacity-100` : "h-0 invisible opacity-0"
             } ${!open
-=======
-          className={`${
-            menuOpen === idx ? `h-[${menu.height}] visible opacity-100` : "h-0 invisible opacity-0"
-          } ${
-            !open
->>>>>>> 32c751229c362428aa98efcfddcb6bcfb01a3968
               ? "absolute left-[65px] bg-white w-max shadow-lg py-2 rounded-tr-md rounded-br-md"
               : ""
             } overflow-hidden duration-300`}
@@ -122,17 +78,9 @@ const MenuItem: React.FC<Props> = (props) => {
                 className={` ${!open ? "px-12" : "pl-[65px]"
                   } h-[35px] flex items-center`}
               >
-<<<<<<< HEAD
-                <Link href="/all_orders">
-                  <span onClick={() => { handleNestedMenuClick(idx) }}
-                    className={`${nestedMenuActive === idx ? "text-[#6777ef]" : ""
-                      } flex-1 text-[13px] hover:text-[#6777ef]`}>
-                    {menu.title}
-=======
                 <Link href={nestedMenu?.link} className={`${asPath === nestedMenu?.link ? "text-[#6777ef]" : ""}`}>
                   <span className="flex-1 text-[13px] hover:text-[#6777ef]">
                     {nestedMenu?.title}
->>>>>>> 32c751229c362428aa98efcfddcb6bcfb01a3968
                   </span>
                 </Link>
 
