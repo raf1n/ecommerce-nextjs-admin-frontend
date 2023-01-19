65a9212bfd7cbffb7d796ab5a298001230f712ed
Rahim Uddin Rafin <rahim.rafin.1@gmail.com>
2023-01-19 14:45:17 +0600

    delete-modal-updated-again

--- components/shared/SharedDeleteModal/SharedDeleteModal.tsx
+++ components/shared/SharedDeleteModal/SharedDeleteModal.tsx
@@ -16,38 +16,40 @@ const SharedDeleteModal: React.FC<Props> = (props) => {
       {showModal ? (
         <div className="relative">
           <div className="flex justify-center fixed inset-0 z-50 bg-black bg-opacity-10 backdrop-blur-[1px]">
-            <div className="bg-white px-6 py-6 rounded-md mt-10 shadow h-fit min-w-fit md:w-1/3">
-              <div className="flex justify-between items-center">
-                <h1 className="text-xl  font-bold text-slate-500">
-                  Item Delete Confirmation
-                </h1>
-                <button onClick={() => setShowModal(false)}>
-                  <HiOutlineX className="w-6 h-6 text-gray-500"></HiOutlineX>
-                </button>
-              </div>
-              <div className="px-2">
-                <div className="my-4">
-                  <div className="my-2">
-                    <label
-                      className="text-[#34395e] tracking-[.5px] font-semibold mt-4	text-sm"
-                      htmlFor=""
-                    >
-                      Are you sure you want to Delete?
-                    </label>
-                    {/* <span className='text-red-500 ml-2'>*</span> */}
+            <div className="bg-white rounded-md mt-10 shadow h-fit min-w-fit md:w-1/3">
+              <div className="px-6 py-6 text-[#6c757d]">
+                <div className="flex justify-between items-center">
+                  <h1 className="text-xl  font-bold text-slate-500">
+                    Item Delete Confirmation
+                  </h1>
+                  <button onClick={() => setShowModal(false)}>
+                    <HiOutlineX className="w-6 h-6 text-gray-500"></HiOutlineX>
+                  </button>
+                </div>
+                <div className="px-2">
+                  <div className="my-4">
+                    <div className="my-2">
+                      <label
+                        className=" tracking-[.5px]  mt-4	text-sm"
+                        htmlFor=""
+                      >
+                        Are you sure you want to Delete?
+                      </label>
+                      {/* <span className='text-red-500 ml-2'>*</span> */}
+                    </div>
                   </div>
                 </div>
               </div>
-              <div className="flex justify-end gap-2 p-2">
+              <div className="flex justify-end gap-2 px-3 py-4 bg-gray-50 rounded">
                 <button
                   onClick={() => setShowModal(false)}
-                  className="bg-red-700 hover:bg-red-600 text-white text-sm py-2 px-4 rounded"
+                  className="bg-red-600 hover:bg-red-500 text-white text-sm py-2 px-4 rounded shadow-[0_2px_6px_#fd9b96]"
                 >
                   Close
                 </button>
                 <button
                   onClick={() => setShowModal(false)}
-                  className="bg-blue-700 hover:bg-blue-600 text-white text-sm py-2 px-4 rounded"
+                  className="bg-blue-700 hover:bg-blue-600 text-white text-sm py-2 px-4 rounded shadow-[0_2px_6px_#acb5f6]"
                 >
                   Yes, Delete
                 </button>
