cab26ecad4bf28c5fba9bd3ddc0d760ea35ce79c
Rahim Uddin Rafin <rahim.rafin.1@gmail.com>
2023-02-12 10:27:11 +0600

    prodct-page-cmnt-deleted

--- components/pages/AdminPage/Dashboard/ManageProducts/Products/Products.tsx
+++ components/pages/AdminPage/Dashboard/ManageProducts/Products/Products.tsx
@@ -267,20 +267,6 @@ const Products: React.FC<Props> = (props) => {
                       deleteModalSlug={deleteModalSlug}
                       setDeleteModalSlug={setDeleteModalSlug}
                     ></SharedDeleteModal>
-                    {/* <div className="px-5 py-5 bg-white border-t flex justify-between">
-                      <span className="text-xs xs:text-sm text-gray-900">
-                        Showing 1 to 4 of 50 Entries
-                      </span>
-                      <div className="inline-flex mt-2 xs:mt-0">
-                        <button className="text-sm text-indigo-50 transition duration-150 hover:bg-indigo-500 bg-indigo-600 font-semibold py-2 px-4 rounded-l">
-                          Prev
-                        </button>
-                        &nbsp; &nbsp;
-                        <button className="text-sm text-indigo-50 transition duration-150 hover:bg-indigo-500 bg-indigo-600 font-semibold py-2 px-4 rounded-r">
-                          Next
-                        </button>
-                      </div>
-                    </div> */}
                     <div className="px-5 py-5  border-t flex justify-between">
                       <div>
                         <span className="text-xs xs:text-sm text-gray-900">
