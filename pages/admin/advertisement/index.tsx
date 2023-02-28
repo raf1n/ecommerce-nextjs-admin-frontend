b74e7cbdf3545a5c0f2f849e3a3b3c99b03b969c
Rahim Uddin Rafin <rahim.rafin.1@gmail.com>
2023-02-28 13:16:53 +0600

    l

--- pages/admin/advertisement/index.tsx
+++ pages/admin/advertisement/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import { controller } from "../../src/state/StateController";
-import Advertisement from "../../components/pages/AdminPage/Dashboard/Advertisement/Advertisement";
+import { controller } from "../../../src/state/StateController";
+import Advertisement from "../../../components/pages/AdminPage/Dashboard/Advertisement/Advertisement";
 
 interface Props {}
 
