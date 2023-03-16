619e6781b7697001da4c8453531aee2f4bfd5955
comconToukir <ahmdtoukir@gmail.com>
2023-03-16 09:31:26 +0600

    tiptap try

--- components/pages/AdminPage/Dashboard/Blogs/SharedTiptap.tsx
+++ components/pages/AdminPage/Dashboard/Blogs/SharedTiptap.tsx
@@ -7,7 +7,7 @@ import { EditorContent, useEditor } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
 
 import { controller } from "../../../../../src/state/StateController";
-import * as styles from "./SharedTiptap.module.css";
+// import styles from "./SharedTiptap.module.css";
 
 interface Props {}
 
@@ -153,9 +153,9 @@ const SharedTiptap: React.FC<Props> = (props) => {
 
   const editor = useEditor({
     extensions: [
-      Color.configure({ types: [TextStyle.name, ListItem.name] }),
+      // Color.configure({ types: [TextStyle.name, ListItem.name] }),
       //@ts-ignore
-      TextStyle.configure({ types: [ListItem.name] }),
+      // TextStyle.configure({ types: [ListItem.name] }),
       StarterKit.configure({
         bulletList: {
           keepMarks: true,
@@ -167,7 +167,41 @@ const SharedTiptap: React.FC<Props> = (props) => {
         },
       }),
     ],
-    content: "<p>Hello World!</p>",
+    editorProps: {
+      attributes: {
+        class:
+          "prose dark:prose-invert prose-sm sm:prose-base lg:prose-lg xl:prose-2xl m-5 focus:outline-none",
+      },
+    },
+    content: `<h2>
+                Hi there,
+              </h2>
+              <p>
+                this is a basic <em>basic</em> example of <strong>tiptap</strong>. Sure, there are all kind of basic text styles you’d probably expect from a text editor. But wait until you see the lists:
+              </p>
+              <ul>
+                <li>
+                  That’s a bullet list with one …
+                </li>
+                <li>
+                  … or two list items.
+                </li>
+              </ul>
+              <p>
+                Isn’t that great? And all of that is editable. But wait, there’s more. Let’s try a code block:
+              </p>
+            <pre><code class="language-css">body {
+            display: none;
+            }</code></pre>
+              <p>
+                I know, I know, this is impressive. It’s only the tip of the iceberg though. Give it a try and click a little bit around. Don’t forget to check the other examples too.
+              </p>
+              <blockquote>
+                Wow, that’s amazing. Good work, boy! 👏
+                <br />
+                — Mom
+              </blockquote>
+            `,
   });
 
   return (
